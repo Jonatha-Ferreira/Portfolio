2dea4e3d56bba4f43d69a3d3f9bbdaecc1ec2a03
Jonatha da Silva Ferreira <91916709+Jonatha-Ferreira@users.noreply.github.com>
2024-01-22 12:11:32 -0300

    CalculadorIMC.js

--- CalculadorIMC.js
+++ CalculadorIMC.js
@@ -1,20 +1,20 @@
 function imc() {
 
 var peso = parseFloat(prompt('Diga seu peso em Kg: ')).toFixed(2);
-var altura = parseFloat(prompt('Digite sua altura em metros: ')).toFixed(2);
+var altura = parseFloat(prompt('Digite sua altura em metros(1.74): ')).toFixed(1);
 var sexo = prompt('Digite seu sexo "m" (masculino) ou "f" (femenino): ') ;
 var imcR = peso/(altura*altura);
-var tbIMC = ['Abaixo do normal','Normal', 'Obesidade leve', 'Obesidade moderada','Obesidade mórbida'] 
-  
+var tbIMC = ['','Abaixo do normal','Normal', 'Obesidade leve', 'Obesidade moderada','Obesidade mórbida'] 
+
   if(sexo != 'm' && sexo != 'f') {
         'dado inválido! digite um novo valor!' + console.log(imc())
       } 
   else { 
   alert('Seu IMC é: ' + imcR.toFixed(2));  
   if (sexo == 'm') {
-    
+
     if (imcR < 20){
-      alert(tbIMC[0])
+      alert(tbIMC[1])
     }
     else if (imcR < 25) {
       alert(tbIMC[2])
@@ -29,11 +29,11 @@ var tbIMC = ['Abaixo do normal','Normal', 'Obesidade leve', 'Obesidade moderada'
       alert(tbIMC[5])
     }
   }
-    
+
   else if (sexo == 'f') {
-      
+
       if (imcR < 19){
-      alert(tbIMC[0])
+      alert(tbIMC[1])
     }
     else if (imcR < 24) {
       alert(tbIMC[2])
@@ -47,7 +47,7 @@ var tbIMC = ['Abaixo do normal','Normal', 'Obesidade leve', 'Obesidade moderada'
     else if (imcR > 39) {
       alert(tbIMC[5])
     }
- 
+
     };
   };  
 }console.log(imc()); 
